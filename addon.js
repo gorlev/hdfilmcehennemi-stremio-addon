f3b7f052c4e98d87d24749d46c3e108f92f9e19e
gorlev <54553199+gorlev@users.noreply.github.com>
2021-04-01 20:46:09 +0300

    Auto-commit

--- addon.js
+++ addon.js
@@ -4,7 +4,7 @@ const { scraper } = require("./scraper")
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
 	"id": "community.HDfilmCehennemi",
-	"version": "0.0.1",
+	"version": "0.0.2",
 	"catalogs": [],
 	"resources": [
 		"stream"
@@ -30,6 +30,7 @@ builder.defineStreamHandler(async ({type, id}) => {
 	let episode = id.split(":")[2]
 	
 	const stream = await scraper(videoId, type, season, episode);
+	
 	return Promise.resolve({ streams: stream })
 })
 
