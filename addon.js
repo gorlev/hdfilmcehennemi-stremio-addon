b2791d2b2d94ccbfd04e198180ec6dc3cbe6cb2f
gorlev <54553199+gorlev@users.noreply.github.com>
2021-09-24 20:45:58 +0300

    v2

--- addon.js
+++ addon.js
@@ -1,26 +1,22 @@
 const { addonBuilder } = require("stremio-addon-sdk")
-const { scraper } = require("./scraper")
+const mainScraper = require("./lib/mainScraper")
 
-// Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
-	"id": "community.HDfilmCehennemi",
-	"version": "0.0.5",
+	"id": "community.hdfilmcehennemi",
+	"version": "0.0.6",
 	"catalogs": [],
-	"resources": [
-		"stream"
-	],
-	"types": [
-		"movie",
-		"series"
-	],
-	"name": "HDfilmCehennemi",
+	"resources": ["stream"],
+	"types": ["movie","series"],
+	"name": "hdfilmcehennemi",
 	"description": "HDFilmCehennemi Stremio Addon brings all the HTTP streams from hdfilmcehennemi.net to Stremio with Turkish dubbing option.",
-	"logo": "https://www.hdfilmcehennemi.net/favicon-new2.ico",
-	"idPrefixes": [
-		"tt"
-	]
+	"logo": "https://www.hdfilmcehennemi.tv/assets/front/icon/favicon.ico",
+	"idPrefixes": ["tt"]
 }
 const builder = new addonBuilder(manifest)
+
+const CACHE_MAX_AGE = 4 * 60 * 60; // 4 hours in seconds
+const STALE_REVALIDATE_AGE = 4 * 60 * 60; // 4 hours
+const STALE_ERROR_AGE = 7 * 24 * 60 * 60; // 7 days
 
 builder.defineStreamHandler(async ({type, id}) => {
 	console.log("request for streams: "+type+" "+id)
@@ -29,9 +25,10 @@ builder.defineStreamHandler(async ({type, id}) => {
 	let season = id.split(":")[1]
 	let episode = id.split(":")[2]
 	
-	const stream = await scraper(videoId, type, season, episode);
-	
-	return Promise.resolve({ streams: stream })
+	const stream = await mainScraper(videoId, type, season, episode)
+	// console.log(stream)
+	return Promise.resolve({ streams: stream, cacheMaxAge: CACHE_MAX_AGE, staleRevalidate: STALE_REVALIDATE_AGE, staleError: STALE_ERROR_AGE})
+
 })
 
 module.exports = builder.getInterface()
